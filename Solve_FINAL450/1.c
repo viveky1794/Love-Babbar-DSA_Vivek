f5f8eda19698678f4e4ef334b1ce48c1af4856d1
vivek <viveky1794@gmail.com>
2020-10-23 23:11:06 +0530

    Test

---
+++ Solve_FINAL450/1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+
+
+}
+
+void reverseArray(int *arr, int size)
+{
+
+	int i=0, j = size-1;
+
+	while( i < j )
+	{
+		swap( &arr[i++], &arr[j--]);
+
+
+	}
+
+}
+
+
+int main()
+{
+	int a[10] = {0,1,2,3,4,5,6,7,8,9};
+	int size = sizeof(a)/sizeof(int);
+	reverseArray(a,size);
+
+	for(int i=0; i< 10; i++)
+		printf("%d, ", a[i]);
+
+
+}
+
+
+
+
